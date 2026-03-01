55a2cfccce153fa6603aa4d648810fd38e316ded
Jerome Angibaud <djeangdev@yahoo.fr>
2026-03-01 21:58:48 +0100

    Refactor JavaFX plugin to simplify module path and module names handling

--- jeka-src/_dev/Custom.java
+++ jeka-src/_dev/Custom.java
@@ -1,16 +1,18 @@
 package _dev;
 
+import dev.jeka.core.tool.JkDep;
 import dev.jeka.core.tool.JkJekaVersionRanges;
 import dev.jeka.core.tool.JkPostInit;
 import dev.jeka.core.tool.KBean;
 import dev.jeka.core.tool.builtins.base.BaseKBean;
 
+@JkDep("org.junit.jupiter:junit-jupiter:6.0.3")
 class Custom extends KBean {
 
     @JkPostInit
     private void postInit(BaseKBean baseKBean) {
         JkJekaVersionRanges.setCompatibilityRange(baseKBean.getManifest(),
-                "0.11.55",
+                "0.11.59",
                 "https://raw.githubusercontent.com/jeka-dev/javafx-plugin/master/breaking_versions.txt");
     }
 
